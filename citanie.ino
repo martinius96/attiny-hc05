6ba9aee219ebdf4eb0917699dc159589e8359428
Martin <martinius96@gmail.com>
2019-05-17 00:37:01 +0200

    format

--- citanie.ino
+++ citanie.ino
@@ -1,9 +1,7 @@
-// Autor: Martin Chlebovec alias: martinius96
-// Podpora: https://www.paypal.me/Chlebovec
+// HC-05 - AtTiny85 tester - basic commands
+// Autor: Martin Chlebovec
 // Osobný web: https://arduino.php5.sk
-// Email kontakt: martinius96@gmail.com
-// Facebook kontakt: 100001242570317
-// Používajte v súhade s licenciou!
+// Email: martinius96@gmail.com
 #define RX 3
 #define TX 4
 #define pinLED 0
@@ -12,26 +10,26 @@ SoftwareSerial bluetooth(TX, RX);
 
 void setup() {
   bluetooth.begin(9600);
- pinMode(pinLED, OUTPUT);
+  pinMode(pinLED, OUTPUT);
 }
 
 void loop() {
   byte BluetoothData;
- if (bluetooth.available() > 0) {
-    
-    BluetoothData=bluetooth.read();
+  if (bluetooth.available() > 0) {
+
+    BluetoothData = bluetooth.read();
     switch (BluetoothData) {
       case '0':
-      digitalWrite(pinLED, LOW);
+        digitalWrite(pinLED, LOW);
         bluetooth.println("Vypni LED diodu.");
         break;
       case '1':
-       digitalWrite(pinLED, HIGH);
+        digitalWrite(pinLED, HIGH);
         bluetooth.println("Zapni LED diodu.");
         break;
       case 'c':
         bluetooth.print("System bezi: ");
-        bluetooth.print(millis()/1000);
+        bluetooth.print(millis() / 1000);
         bluetooth.println(" sekund.");
         break;
       case '\r':
